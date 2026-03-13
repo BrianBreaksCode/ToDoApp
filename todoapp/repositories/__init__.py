e615239108db589af6a167dfb6828dff675e6d72
Brian van Batenburg <55662360+BrianBreaksCode@users.noreply.github.com>
2026-03-13 17:40:06 +0100

    (repositories): add TodoListRepository

---
+++ todoapp/repositories/__init__.py
@@ -0,0 +1,4 @@
+from todoapp.repositories.todo_list_repository import TodoListRepository
+from todoapp.repositories.sqlite_todo_list_repository import SqliteTodoListRepository
+
+__all__ = ["TodoListRepository", "SqliteTodoListRepository"]
